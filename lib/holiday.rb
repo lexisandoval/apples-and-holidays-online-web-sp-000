686473fa29d323e5fa2cca9eec23bc8f16d72efb
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-01-18 20:11:32 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -86,7 +86,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_hash.each do |season, holiday|
-    season.each do |holiday, supplies|
+    holiday.each do |holiday, supplies|
       puts holiday
     end
   end
