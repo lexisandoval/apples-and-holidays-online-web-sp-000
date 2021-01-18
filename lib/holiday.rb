1c589142d033fd0fa9edf24d42c2af6217dcc5b5
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-01-18 20:33:02 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,12 +65,17 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 
-  holiday_hash.each do |season, holiday|
-    puts "#{season.capitalize}:"
-    holidays.each do |holiday, supplies|
-      puts"  #{holiday.to_s.split('_').map {|w| w.capitalize }.join(' ') }: #{supplies.join(", ")}"
+  holiday_hash.each do |season, holiday_with_supplies|
+      puts "#{season.capitalize}:"
+      holiday_with_supplies.each do |holiday, supplies|
+        holiday_name = holiday.to_s.split("_")
+        holiday_name.each do |word|
+          word.capitalize!
+        end
+        new_holiday = holiday_name.join(" ")
+        puts "  #{new_holiday}: #{supplies.join(", ")}"
+      end
     end
-  end
 
 end
 
