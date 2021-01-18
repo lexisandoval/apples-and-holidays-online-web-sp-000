fe3310d4a10276c12588c05fc21d53c397c05473
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-01-18 20:01:53 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,31 +65,11 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holiday|
-
     if season == :winter
-      puts "Winter:"
-      if holiday == :christmas
-        puts "Christmas:"
-        holiday.each do |holiday, supplies|
-          supplies.each do |supplies|
-            puts supplies
-          end
-        end
-      elsif holiday == :new_years
-        puts "New Years:"
-        holiday.each do |holiday,supplies|
-          supplies.each do |supplies|
-            puts supplies
-          end
-        end
-      end
-    elsif season == :summer
-      puts "Summer:"
-
-
-    end
-end
-end
+      winter = :winter.to_s
+      winter.capitalize!
+      puts winter
+  end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
