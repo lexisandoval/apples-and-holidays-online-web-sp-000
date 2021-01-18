f3b235ea0551b3e67bbe7421cf9d7170a0b608d6
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-01-18 19:46:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,6 +64,17 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  holiday_hash.each do |season, holiday|
+    temp = []
+    if season == :winter
+      puts "Winter:"
+      if holiday == :christmas
+        holiday.each do |holiday, supplies|
+          temp << supplies
+          temp.join(", ")
+
+        end
+        els
 
 end
 
